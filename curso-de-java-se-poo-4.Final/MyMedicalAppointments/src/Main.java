8aaa03562a76a25b6a7a3086dc8fa2f2cea26eb7
fabith95 <fabith95@gmail.com>
2023-06-01 14:20:58 -0500

    Herencia and Polimorfismo

--- curso-de-java-se-poo-4.Final/MyMedicalAppointments/src/Main.java
+++ curso-de-java-se-poo-4.Final/MyMedicalAppointments/src/Main.java
@@ -1,21 +1,28 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
 
         //showMenu();
-        Doctor myDoctor = new Doctor("fabith", "pediatria");
+        Doctor myDoctor = new Doctor("fabith", "fabith95@hmail.co");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "8pm");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
 
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        System.out.println(myDoctor);
+
+//        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()
+//             ) {
+//            System.out.println(aA.getDate() + " " + aA.getTime());
+//        }
+
+        System.out.println();
 
         Patient patient = new Patient("stiven", "stiven95@gmail.com");
+        System.out.println(patient);
 
-        patient.setHeight(1.7);
-        System.out.println(patient.getHeight());
-
-        patient.setWeight(75);
-        System.out.println(patient.getWeight());
 
     }
 
--- curso-de-java-se-poo-4.Final/MyMedicalAppointments/src/Patient.java
+++ curso-de-java-se-poo-4.Final/MyMedicalAppointments/src/Patient.java
@@ -1,17 +1,14 @@
-public class Patient {
-    int id;
-    private String name;
-    private String email;
-    private String address;
-    private String phoneNumber;
+import ui.User;
+
+public class Patient extends User {
+
     private String birthday;
     private double weight;
     private double height;
     private String blood;
 
     Patient(String name, String email){
-        this.name = name;
-        this.email = email;
+        super(name, email);
     }
 
     public String getWeight() {
@@ -30,46 +27,6 @@ public class Patient {
         this.height = height;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
     public String getBirthday() {
         return birthday;
     }
@@ -84,5 +41,11 @@ public class Patient {
 
     public void setBlood(String blood) {
         this.blood = blood;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\nbirthday: " +  getBirthday() + "\nweight: " +
+                getWeight() + "\nheight: " +  getHeight() + "\nblood: " + getBlood();
     }
 }
